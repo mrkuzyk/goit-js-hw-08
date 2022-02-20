76a543993ae3e362034d5c1475a6587255891f00
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-20 19:32:04 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import {galleryItems} from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+
+//? cтворюю розмітку з вхідного масиву
+const makePreviewImageGallery = ({preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+        <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+    </a>`
+}
+
+//? перебираю масив і позбуваюся його
+const elements = galleryItems.map(makePreviewImageGallery).join('');
+
+//? добавляю розмітку в готовий хтмл
+galleryEl.insertAdjacentHTML('beforeend', elements);
+
+//? підключена бібліотека на модалку
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', //? змінено текст щоб брався з "альт"
+    captionDelay: 250, //? затримка появи тексту 250 мс
+});
