6e037dbfdaa5e66640f176d8ecac4fdfeedce41e
Marian Kuzyk <mariankuzyk@gmail.com>
2022-02-21 23:52:41 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onListenForm);
+form.addEventListener('submit', onPressButton);
+
+let formData = {};
+
+
+function onListenForm(e) {
+    formData.email = form.elements.email.value;
+    formData.message = form.elements.message.value;
+    // console.log(formData);
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+
+
+function onPressButton (e) {
+    e.preventDefault();
+
+    e.currentTarget.reset(); //? очищує поля після відправки
+    console.log(formData); //? виводить введене після відправки
+}
